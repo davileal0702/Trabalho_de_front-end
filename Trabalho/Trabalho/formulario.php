12a872dc20245a078b1d50c6bdcdb0074622adf1
Davi Leal <davileal0702@gmail.com>
2025-04-03 21:01:07 -0300

    top

---
+++ Trabalho/Trabalho/formulario.php
@@ -0,0 +1,10 @@
+<?php
+$campo1 = $_POST['campo1'];
+$campo2 = $_POST['campo2'];
+$campo3 = $_POST['campo3'];
+$campo4 = $_POST['campo4'];
+$campo5 = $_POST['campo5'];
+
+header("Location: index.html");
+exit();
+?>
